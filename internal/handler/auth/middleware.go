e46d4329532b9957e0d38335619a2b1d9319642b
alex <nomfodm@gmail.com>
2024-10-28 21:10:40 +0300

    auth middleware error response update

--- internal/handler/auth/middleware.go
+++ internal/handler/auth/middleware.go
@@ -15,35 +15,41 @@ func NewAuthMiddleware(uc usecase.AuthUseCase) gin.HandlerFunc {
 	return (&AuthMiddleware{uc: uc}).Handle
 }
 
-func notAuthorized(ctx *gin.Context, err string) {
+func notAuthorized(ctx *gin.Context, err string, detail string) {
 	ctx.AbortWithStatusJSON(401, gin.H{
-		"error": err,
+		"error":  err,
+		"detail": detail,
 	})
 }
 
 func (m *AuthMiddleware) Handle(ctx *gin.Context) {
 	authorizationHeader := ctx.GetHeader("Authorization")
 	if authorizationHeader == "" {
-		notAuthorized(ctx, "provide access token string")
+		notAuthorized(ctx, "Unauthorized", "provide access token string")
 		return
 	}
 
 	headerSplitted := strings.Split(authorizationHeader, " ")
 	if len(headerSplitted) != 2 {
-		notAuthorized(ctx, "provide corrent access token string")
+		notAuthorized(ctx, "Unauthorized", "provide corrent access token string")
 		return
 	}
 
 	if headerSplitted[0] != "Bearer" {
-		notAuthorized(ctx, "access token type must be bearer")
+		notAuthorized(ctx, "Unauthorized", "access token type must be bearer")
 		return
 	}
 
 	accessToken := headerSplitted[1]
 
+	if accessToken == "null" {
+		notAuthorized(ctx, "Unauthorized", "provide corrent access token string")
+		return
+	}
+
 	user, err := m.uc.RetrieveUserWithAccessToken(accessToken)
 	if err != nil {
-		notAuthorized(ctx, err.Error())
+		notAuthorized(ctx, "Unauthorized", err.Error())
 		return
 	}
 
