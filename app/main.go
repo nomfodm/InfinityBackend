9d290f59249d7b09e46a8ee51907d862c777bfa2
alex <nomfodm@gmail.com>
2024-10-28 21:00:20 +0300

    cors

--- app/main.go
+++ app/main.go
@@ -15,6 +15,22 @@ import (
 	"gorm.io/gorm"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -30,6 +46,8 @@ func main() {
 	db.AutoMigrate(&entity.Skin{}, &entity.Cape{}, &entity.MinecraftCredential{}, &entity.User{}, &entity.RefreshToken{})
 
 	router := gin.Default()
+
+	router.Use(CORSMiddleware())
 
 	postgresUserRepository := postgresRepository.NewPostgresUserRepository(db)
 	authUseCaseImpl := usecase.NewAuthUseCaseImpl(postgresUserRepository)
